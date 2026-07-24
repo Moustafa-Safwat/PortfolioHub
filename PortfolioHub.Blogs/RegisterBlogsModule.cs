d5c095080278736bbcb290cd07aaed39ca5116c2
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-24 23:10:36 +0400

    feat: init blogs project folders with module register

---
+++ PortfolioHub.Blogs/RegisterBlogsModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace PortfolioHub.Blogs;
+
+public static class RegisterBlogsModule
+{
+    public static IServiceCollection AddBlogsModule(this IServiceCollection service,
+      IConfiguration configuration, IList<Assembly> assemblies)
+    {
+
+        return service;
+    }
+}
--- PortfolioHub.Web/Program.cs
+++ PortfolioHub.Web/Program.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using PortfolioHub.Achievements;
+using PortfolioHub.Blogs;
 using PortfolioHub.Notification;
 using PortfolioHub.Projects;
 using PortfolioHub.SharedKernal.Domain.Interfaces;
@@ -28,7 +29,8 @@ builder.Services
     .AddUsersModule(builder.Configuration, assemblies)
     .AddProjectsModule(builder.Configuration, assemblies)
     .AddAchievementsModule(builder.Configuration, assemblies)
-    .AddNotificationModule(builder.Configuration, assemblies);
+    .AddNotificationModule(builder.Configuration, assemblies)
+    .AddBlogsModule(builder.Configuration, assemblies);
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
