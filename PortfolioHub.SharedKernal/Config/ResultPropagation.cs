6a0759a97a2cd1d343cd3594512b0c594ca598f6
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-26 16:32:31 +0400

    feat: implement result error propagation in shared kernal to can reuse it in all modules

---
+++ PortfolioHub.SharedKernal/Config/ResultPropagation.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Result;
+
+namespace PortfolioHub.SharedKernal.Config;
+
+public static class ResultPropagation
+{
+    public static Result<U> PropagateFailure<T, U>(this Result<T> result) =>
+        result.Status switch
+        {
+            ResultStatus.Error => Result<U>.Error(new ErrorList(result.Errors)),
+            ResultStatus.Forbidden => Result<U>.Forbidden(result.Errors.ToArray()),
+            ResultStatus.Unauthorized => Result<U>.Unauthorized(result.Errors.ToArray()),
+            ResultStatus.Invalid => Result<U>.Invalid(result.ValidationErrors),
+            ResultStatus.NotFound => Result<U>.NotFound(result.Errors.ToArray()),
+            ResultStatus.NoContent => Result<U>.NoContent(),
+            ResultStatus.Conflict => Result<U>.Conflict(result.Errors.ToArray()),
+            ResultStatus.CriticalError => Result<U>.CriticalError(result.Errors.ToArray()),
+            ResultStatus.Unavailable => Result<U>.Unavailable(result.Errors.ToArray()),
+            _ => throw new InvalidOperationException($"Unexpected ResultStatus: {result.Status}")
+        };
+}
