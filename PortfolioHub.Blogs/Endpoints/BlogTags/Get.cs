8c3011288ebc92d13647303a8eee6fa1e875de78
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-26 20:04:58 +0400

    feat: implement endpoint to get tags

---
+++ PortfolioHub.Blogs/Endpoints/BlogTags/Get.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.Result;
+using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using PortfolioHub.Blogs.Usecases.BlogTags.Get;
+using System.Collections.ObjectModel;
+
+namespace PortfolioHub.Blogs.Endpoints.BlogTags;
+
+internal class Get
+(
+    ISender sender
+) : EndpointWithoutRequest<Result<ReadOnlyCollection<TagsDto>>>
+{
+    public override void Configure()
+    {
+        Get("/blog-tags");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var getBlogTagsQuery = new GetBlogTagsQuery();
+        var result = await sender.Send(getBlogTagsQuery, ct);
+
+        if (!result.IsSuccess)
+        {
+            var response = Result.Error(new ErrorList(result.Errors));
+            await SendAsync(response, StatusCodes.Status400BadRequest, cancellation: ct);
+            return;
+        }
+
+        await SendAsync(result, cancellation: ct);
+    }
+}
---
+++ PortfolioHub.Blogs/Usecases/BlogTags/Get/GetBlogTagsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Result;
+using PortfolioHub.Blogs.Domain.Entities;
+using PortfolioHub.Blogs.Endpoints.BlogTags;
+using PortfolioHub.SharedKernal.Config;
+using PortfolioHub.SharedKernal.Domain.Interfaces;
+using System.Collections.ObjectModel;
+using ValidBuild.Sharedkernal.Domain.CQRS;
+
+namespace PortfolioHub.Blogs.Usecases.BlogTags.Get;
+
+internal sealed class GetBlogTagsQueryHandler
+(
+    IEntityRepo<BlogPostTag> blogTagRepo
+) : IQueryHandler<GetBlogTagsQuery, ReadOnlyCollection<TagsDto>>
+{
+    public async Task<Result<ReadOnlyCollection<TagsDto>>> Handle(GetBlogTagsQuery request, CancellationToken cancellationToken)
+    {
+        var blogTagResult = await blogTagRepo.GetAllAsync(1, 100, cancellationToken);
+        if (!blogTagResult.IsSuccess)
+            return blogTagResult.PropagateFailure<IReadOnlyList<BlogPostTag>, ReadOnlyCollection<TagsDto>>();
+
+        var tagsDtos = blogTagResult.Value
+            .Select(tag => new TagsDto(tag.Id, tag.Name))
+            .ToList()
+            .AsReadOnly();
+
+        return Result.Success(tagsDtos);
+    }
+}
