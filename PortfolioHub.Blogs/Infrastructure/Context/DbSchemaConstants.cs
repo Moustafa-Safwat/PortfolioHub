941c47c83a30df3d9b2ba350d6e1b8a20e54c00f
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-26 10:34:47 +0400

    feat: add dbcontext for blogs

---
+++ PortfolioHub.Blogs/Infrastructure/Context/DbSchemaConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace PortfolioHub.Blogs.Infrastructure.Context;
+
+internal static class DbSchemaConstants
+{
+    public const string BLOGS_SCHEMA = "Blogs";
+}
--- PortfolioHub.Blogs/RegisterBlogsModule.cs
+++ PortfolioHub.Blogs/RegisterBlogsModule.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PortfolioHub.Blogs.Infrastructure.Context;
 using System.Reflection;
 
 namespace PortfolioHub.Blogs;
@@ -9,7 +10,9 @@ public static class RegisterBlogsModule
     public static IServiceCollection AddBlogsModule(this IServiceCollection service,
       IConfiguration configuration, IList<Assembly> assemblies)
     {
+        service.AddSqlServer<BlogsDbContext>(configuration.GetConnectionString("BlogsDb"));
 
+        assemblies.Add(typeof(RegisterBlogsModule).Assembly);
         return service;
     }
 }
