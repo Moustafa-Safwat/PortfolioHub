fda7b1a09d071a49d4d1d91aa7800c26ce9d5ffb
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-26 12:46:45 +0400

    feat: add db seeder for blog tags

---
+++ PortfolioHub.Blogs/Infrastructure/DbSeed/DbBlogTagSeeder.cs
@@ -0,0 +1,33 @@
+﻿using PortfolioHub.Blogs.Domain.Entities;
+using PortfolioHub.SharedKernal.Domain.Interfaces;
+using PortfolioHub.Users.Domain.Interfaces;
+
+namespace PortfolioHub.Blogs.Infrastructure.DbSeed;
+
+internal sealed class DbBlogTagSeeder
+(
+    IEntityRepo<BlogPostTag> tagRepo
+) : IDbSeeder
+{
+    private readonly IEnumerable<string> _blogTags = ["BIM", "IFC", "Revit API", "CAD API", "C#", "Backend", "Architecture", "Docker", "AI", "Structure Analysis", "AR", "WPF", "Nginx"];
+
+    public async Task SeedAsync()
+    {
+        var getAllTagsResult = await tagRepo.GetAllAsync(1, 100);
+        if (!getAllTagsResult.IsSuccess)
+            throw new InvalidOperationException();
+
+        var getAllTags = getAllTagsResult.Value;
+        var tagsToAdd = _blogTags.Except(getAllTags.Select(tag => tag.Name));
+        if (tagsToAdd.Any())
+        {
+            tagsToAdd.ToList()
+                .ForEach(async (tag) =>
+                {
+                    var blogPostTag = new BlogPostTag(tag);
+                    await tagRepo.AddAsync(blogPostTag);
+                });
+            await tagRepo.SaveChangesAsync();
+        }
+    }
+}
--- PortfolioHub.Blogs/RegisterBlogsModule.cs
+++ PortfolioHub.Blogs/RegisterBlogsModule.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PortfolioHub.Blogs.Infrastructure.Context;
+using PortfolioHub.Blogs.Infrastructure.DbSeed;
+using PortfolioHub.Blogs.Infrastructure.EFRepository;
+using PortfolioHub.SharedKernal.Domain.Entities;
+using PortfolioHub.SharedKernal.Domain.Interfaces;
 using System.Reflection;
 
 namespace PortfolioHub.Blogs;
@@ -11,6 +15,22 @@ public static class RegisterBlogsModule
       IConfiguration configuration, IList<Assembly> assemblies)
     {
         service.AddSqlServer<BlogsDbContext>(configuration.GetConnectionString("BlogsDb"));
+
+        service.AddScoped<DbBlogTagSeeder>();
+
+        var entityTypes = typeof(RegisterBlogsModule).Assembly
+       .GetTypes()
+       .Where(t => t.IsClass
+       && !t.IsAbstract
+       && typeof(BaseEntity).IsAssignableFrom(t));
+
+        foreach (var entityType in entityTypes)
+        {
+            var repoInterface = typeof(IEntityRepo<>).MakeGenericType(entityType);
+            var repoImplementation = typeof(EFEntityRepo<>).MakeGenericType(entityType);
+            service.AddScoped(repoInterface, repoImplementation);
+        }
+
 
         assemblies.Add(typeof(RegisterBlogsModule).Assembly);
         return service;
