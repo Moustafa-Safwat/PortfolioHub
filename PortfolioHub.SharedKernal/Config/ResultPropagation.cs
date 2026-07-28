13450ee20110908d4c050c878be17770ac3cfc95
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-28 23:29:55 +0400

    fix: handel return result object for PropagateFailure extension method

--- PortfolioHub.SharedKernal/Config/ResultPropagation.cs
+++ PortfolioHub.SharedKernal/Config/ResultPropagation.cs
@@ -18,4 +18,19 @@ public static class ResultPropagation
             ResultStatus.Unavailable => Result<U>.Unavailable(result.Errors.ToArray()),
             _ => throw new InvalidOperationException($"Unexpected ResultStatus: {result.Status}")
         };
+
+    public static Result PropagateFailure<T>(this Result<T> result) =>
+       result.Status switch
+       {
+           ResultStatus.Error => Result.Error(new ErrorList(result.Errors)),
+           ResultStatus.Forbidden => Result.Forbidden(result.Errors.ToArray()),
+           ResultStatus.Unauthorized => Result.Unauthorized(result.Errors.ToArray()),
+           ResultStatus.Invalid => Result.Invalid(result.ValidationErrors),
+           ResultStatus.NotFound => Result.NotFound(result.Errors.ToArray()),
+           ResultStatus.NoContent => Result.NoContent(),
+           ResultStatus.Conflict => Result.Conflict(result.Errors.ToArray()),
+           ResultStatus.CriticalError => Result.CriticalError(result.Errors.ToArray()),
+           ResultStatus.Unavailable => Result.Unavailable(result.Errors.ToArray()),
+           _ => throw new InvalidOperationException($"Unexpected ResultStatus: {result.Status}")
+       };
 }
