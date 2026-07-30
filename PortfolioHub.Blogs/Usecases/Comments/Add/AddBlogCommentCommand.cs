a8aa635d7d2774b00c81335aff31a88c054fc36c
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-31 00:10:43 +0400

    feat: add comment feature

---
+++ PortfolioHub.Blogs/Usecases/Comments/Add/AddBlogCommentCommand.cs
@@ -0,0 +1,11 @@
+﻿using ValidBuild.Sharedkernal.Domain.CQRS;
+
+namespace PortfolioHub.Blogs.Usecases.Comments.Add;
+
+internal sealed record AddBlogCommentCommand
+(
+    Guid BlogId,
+    Guid UserId,
+    string Comment,
+    Guid? ParentCommentId
+) : ICommand<Guid>;
