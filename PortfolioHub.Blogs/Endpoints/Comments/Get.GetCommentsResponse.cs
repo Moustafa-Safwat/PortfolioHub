7f645f4e6a17695dc48fa4e3f9ffddd33b2ecc64
moust <moustafa.safwat.iti.43@gmail.com>
2026-07-31 18:40:19 +0400

    feat: get comment for blog

---
+++ PortfolioHub.Blogs/Endpoints/Comments/Get.GetCommentsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace PortfolioHub.Blogs.Endpoints.Comments;
+
+internal sealed record GetCommentsResponse
+(
+    Guid Id,
+    string UserFirstName,
+    string UserLastName,
+    string Comment,
+    DateTime CreatedAtUtc,
+    IReadOnlyCollection<GetCommentsResponse> Replies
+);
