0f59ab15f9ecddc8c8df35a852e10d88a5210ed8
moust <moustafa.safwat.iti.43@gmail.com>
2026-08-01 12:00:40 +0400

    feat: add delete endpoint for comment

---
+++ PortfolioHub.Blogs/Endpoints/Comments/Delete.DeleteCommentValidator.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace PortfolioHub.Blogs.Endpoints.Comments;
+
+internal sealed class DeleteCommentValidator : Validator<DeleteCommentReq>
+{
+    public DeleteCommentValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(request => request.BlogId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Blog ID is required and must be a valid GUID.");
+
+        RuleFor(request => request.CommentId)
+            .NotEqual(Guid.Empty)
+            .WithMessage("Comment ID is required and must be a valid GUID.");
+
+        RuleFor(request => request)
+            .Must(request => request.BlogId != request.CommentId)
+            .WithMessage("Blog ID and comment ID cannot have the same value.");
+    }
+}
---
+++ PortfolioHub.Blogs/Usecases/Comments/Delete/DeleteCommentCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Result;
+using PortfolioHub.Blogs.Domain.Interfaces;
+using PortfolioHub.SharedKernal.Config;
+using ValidBuild.Sharedkernal.Domain.CQRS;
+
+namespace PortfolioHub.Blogs.Usecases.Comments.Delete;
+
+internal sealed class DeleteCommentCommandHandler
+(
+    IBlogsRepo blogsRepo
+) : ICommandHandler<DeleteCommentCommand>
+{
+    public async Task<Result> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+    {
+        var getBlogByIdResult = await blogsRepo.GetByIdAsync(request.BlogId, cancellationToken);
+        if (!getBlogByIdResult.IsSuccess)
+            return getBlogByIdResult.PropagateFailure();
+
+        var blog = getBlogByIdResult.Value;
+        var commnetToDelete = blog.BlogComments.FirstOrDefault(comment => comment.Id == request.CommentId);
+        if (commnetToDelete is null)
+            return Result.NotFound($"Blog comment with id: {request.CommentId} is not found for blog id: {request.BlogId}");
+
+        commnetToDelete.MarkAsDeleted(request.UserId);
+
+        var saveResult = await blogsRepo.SaveChangesAsync(cancellationToken);
+        if (!saveResult.IsSuccess)
+            return saveResult.PropagateFailure();
+
+        return Result.SuccessWithMessage($"Comment with id: {request.CommentId} is deleted successfully");
+    }
+}
