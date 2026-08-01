549a26110f0b1fd1a59750d94b68ba135df47218
moust <moustafa.safwat.iti.43@gmail.com>
2026-08-01 11:03:21 +0400

    feat: get blog by slug endpoint

---
+++ PortfolioHub.Blogs/Usecases/Blogs/GetBySlug/GetBlogBySlugQuery.cs
@@ -0,0 +1,10 @@
+﻿using PortfolioHub.Blogs.Endpoints.Blogs;
+using ValidBuild.Sharedkernal.Domain.CQRS;
+
+namespace PortfolioHub.Blogs.Usecases.Blogs.GetBySlug;
+
+internal sealed record GetBlogBySlugQuery
+(
+    string Slug,
+    Guid UserId
+) : IQuery<BlogDetailsDto>;
