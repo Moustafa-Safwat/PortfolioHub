ac4c7ddfa32e9f2f961a6417806da89c5943cd8e
moust <moustafa.safwat.iti.43@gmail.com>
2026-08-01 11:11:20 +0400

    feat: add slug for get blogs endpoint

--- PortfolioHub.Blogs/Endpoints/Blogs/Get.BlogsReadDto.cs
+++ PortfolioHub.Blogs/Endpoints/Blogs/Get.BlogsReadDto.cs
@@ -5,6 +5,7 @@ namespace PortfolioHub.Blogs.Endpoints.Blogs;
 internal sealed record BlogsReadDto(
     Guid Id,
     string Title,
+    string Slug,
     string CoverImageUrl,
     BlogStatus Status,
     string Description,
